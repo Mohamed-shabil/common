86e786345b4e0ec48896d2b4aa2daad652952827
Mohamed Shabil <mohamedshabil231@gmail.com>
2023-12-20 17:43:17 +0530

    updates

--- src/middlewares/require-auth.ts
+++ src/middlewares/require-auth.ts
@@ -3,6 +3,7 @@ import { NotAutherizedError } from '../errors/not-autherized-error';
 
 export const requireAuth = (req:Request,res:Response, next:NextFunction)=>{
     if(!req.currentUser){
+        console.log('no Current User found');
         throw new NotAutherizedError();
     }
     next();
