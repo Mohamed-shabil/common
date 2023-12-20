58a76c320f90df5f374daea9872caed2dd0223f4
Mohamed Shabil <mohamedshabil231@gmail.com>
2023-12-20 22:49:47 +0530

    updates

--- src/middlewares/current-user.ts
+++ src/middlewares/current-user.ts
@@ -21,12 +21,9 @@ export const currentUser = (
     if(!req.session?.jwt){
         return next();
     } 
-
     try{
         const payload = jwt.verify(req.session.jwt,process.env.JWT_KEY!) as UserPayload;
-
         req.currentUser = payload;
-        
     }catch(err){
     }
     next();
